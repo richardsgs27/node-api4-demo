68e5abfd26b86b48d199f24d67bf6be58e33a3b4
Gigi <gigi@iMacs-iMac.local>
2022-05-27 15:42:58 -0500

    fortunes

--- api/server.js
+++ api/server.js
@@ -1,13 +1,18 @@
 const express = require("express");
-
+const fortunes = require('./fortunes')
 const server = express();
 
-server.get('/', (req, res) => {
-    res.send('<h1>HEROKU DEPLOYMENT!!</h1>')
-})
+server.get("/", (req, res) => {
+  res.send("<h1>HEROKU DEPLOYMENT!!</h1>");
+});
 
-server.get('/node-env', (req, res) => {
-    res.json({NODE_ENV: process.env.NODE_ENV})
-})
+server.get("/node-env", (req, res) => {
+  res.json({ NODE_ENV: process.env.NODE_ENV });
+});
 
-module.exports = server
+server.get("/fortune", (req, res) => {
+ let randomIndex = Math.floor(Math.random() * fortunes.length)
+ res.send(`<h1>${fortunes[randomIndex]}</h1>`)
+  });
+
+module.exports = server;
