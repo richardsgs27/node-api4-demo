90d91abe48c0dd5597d160892a9fe939b6f4cc15
Gigi <gigi@iMacs-iMac.local>
2022-05-27 14:23:55 -0500

    initial commit

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('<h1>HEROKU DEPLOYMENT!!</h1>')
+})
+
+server.get('/node-env', (req, res) => {
+    res.json({NODE_ENV: process.env.NODE_ENV})
+})
+
+module.exports = server;
---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+const server = require("./api/server.js");
+
+const PORT = process.env.PORT;
+
+if(PORT == null) {
+    console.error('no port set')
+} else {
+    server.listen(PORT, () => {
+      console.log(`server is running on port ${PORT}`);
+      console.log(`environment is ${process.env.NODE_ENV}`);
+    });
+}
+
